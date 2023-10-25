bc084e4ae1b42a6ab1c74f79e6bba4ab43589678
Gotcha190 <konradformella190@gmail.com>
2023-10-25 13:59:54 +0200

    Code cleanup. Set main.dart to be router only.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
-import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'dart:async';
+import 'package:gym_rent/WelcomePage.dart';
 
-late StreamSubscription<bool> keyboardSubscription;
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -16,145 +12,17 @@ class MyApp extends StatelessWidget {
     return Sizer(
       builder: (context, orientation, deviceType) {
         return MaterialApp(
+          initialRoute: '/',
           title: 'GymRent',
           theme: ThemeData(
             primarySwatch: Colors.orange,
             fontFamily: 'KeaniaOne',
           ),
-          home: const MyHomePage(title: 'GymRent'),
+          routes: {
+            '/': (ctx) => const WelcomePage(),
+          },
         );
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-             if (MediaQuery.of(context).viewInsets.bottom == 0.0)
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Image.asset('assets/images/main_page_logo.png',
-                      width: 100.w, height: 40.w),
-                  Text(
-                    'GymRent',
-                    style: TextStyle(
-                      fontSize: 30.sp,
-                    ),
-                  ),
-                ],
-              ),
-            Expanded(
-              child: ClipPath(
-                clipper: WaveClipperTwo(reverse: true),
-                child: Container(
-                  height: 50.h,
-                  width: 100.w,
-                  color: const Color(0xFF848E95),
-                  child: ListView(
-                    padding: EdgeInsets.only(top: 10.h, right: 5.w, left: 5.w),
-                      children: [
-                        Center(
-                          child: Text(
-                            "Welcome",
-                            style: TextStyle(
-                                fontSize: 45.sp, color: const Color(0xFFFA9F56)),
-                          ),
-                        ),
-                        Center(
-                          child: Text(
-                            "Login to enjoy GymRent",
-                            style: TextStyle(
-                                fontSize: 12.sp, color: const Color(0xFFD9DCDE)),
-                          ),
-                        ),
-                         SizedBox(height: 5.h),
-                        const TextField(
-                          decoration: InputDecoration(
-                            labelText: "Login",
-                            filled: true,
-                            fillColor: Color(0xFFD9DCDE),
-                          ),
-                        ),
-                        SizedBox(height: 5.h),
-                        const TextField(
-                          decoration: InputDecoration(
-                            labelText: "Password",
-                            filled: true,
-                            fillColor: Color(0xFFD9DCDE),
-                          ),
-                          obscureText: true, // Ukryj hasło
-                        ),
-                        SizedBox(height: 5.h),
-                        SizedBox(
-                          width: double.infinity,
-                          height: 6.5.h,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              ///TODO: Login logic
-                            },
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all(
-                                  const Color(0xFFF77B00)),
-                            ),
-                            child: Text(
-                              "Login",
-                              style: TextStyle(
-                                  fontSize: 15.sp,
-                                  color: const Color(0xFFD9DCDE)),
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 2.h),
-                        Row(
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                ///TODO: Forget password page
-                              },
-                              child: Text(
-                                "Forget password?",
-                                style: TextStyle(
-                                    fontSize: 12.sp,
-                                    color: const Color(0xFFD9DCDE)),
-                              ),
-                            ),
-                            const Spacer(),
-                            GestureDetector(
-                              onTap: () {
-                                ///TODO: Creating account page
-                              },
-                              child: Text(
-                                "create new account",
-                                style: TextStyle(
-                                    fontSize: 12.sp,
-                                    color: const Color(0xFFF77B00)),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-          ],
-        ),
-      ),
     );
   }
 }
